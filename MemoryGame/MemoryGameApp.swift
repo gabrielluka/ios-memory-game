abca11bcd8bacc9bfde069f740d60c9540a0ad6e
Gabriel Lucas <g.lucas.santos@gmail.com>
2021-04-04 19:52:52 -0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Gabriel Lucas Santos on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
